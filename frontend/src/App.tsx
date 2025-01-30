bf081ed9a1392b0d4677d9fdd3876cfde7c00684
martinc <martinc@fktech.net>
2025-01-30 17:39:36 -0300

    Feat: Create Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,25 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
   const [url, setUrl] = useState('');
-
   const handleUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
-      
     const { value } = e.target;          
-
     setUrl(value);
+  };
+
+  const sendUrl = async () => {
+    if (url !== '') {
+      try {
+        const response = await axios.post(`http://localhost:3000/short-url`, { url: url });
+        console.log('Respuesta recibida:', response.data);
+      } catch (error) {
+        console.log('Error al hacer la solicitud:', error);
+      }
+    }
   };
 
   return (
@@ -21,7 +30,7 @@ function App() {
       <div className='main-background-input'>
         <input placeholder='https://vercel.com/' onChange={(e) => handleUrl(e)}/>
         <button>
-          <FontAwesomeIcon icon={faLink} onClick={()=> alert(url)}/>
+          <FontAwesomeIcon icon={faLink} onClick={()=> sendUrl()}/>
         </button>
       </div>
     </div>
